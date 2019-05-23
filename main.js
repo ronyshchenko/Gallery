e534c082425fc4a9dfe0bb0e284502ba0c4bb6bb
ronyshchenko <roma.onyshchenko@gmail.com>
2019-05-23 17:59:47 +0300

    first__commit

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 'use strict';
+    const largeImg = document.getElementById('largeImg');
+    const thumbs = document.getElementById('thumbs');
+
+    
+    thumbs.addEventListener('click', function m(event) {
+        let target = event.target;
+  
+        while (target != this) {
+  
+          if (target.nodeName == 'A') {
+            showThumbnail(target.href, target.title);
+            return false;
+          }
+          target = target.parentNode;
+        }
+      }
+    )
+
+    function showThumbnail(href, title) {
+      largeImg.src = href;
+      largeImg.alt = title;
+      event.stopPropagation()
+    }
+    var imgs = thumbs.getElementsByTagName('img');
+    for (let i = 0; i < imgs.length; i++) {
+      let url = imgs[i].parentNode.href;
+      let img = document.createElement('img');
+      img.src = url;
+    }
